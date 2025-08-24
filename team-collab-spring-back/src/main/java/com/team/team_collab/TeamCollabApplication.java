0d4baff556612b2a2aeece57b10722d1a595c93c
PKBHATIA7777 <parthbhatia7777@gmail.com>
2025-08-24 20:24:35 +0530

    Added Spring Boot project skeleton

---
+++ team-collab-spring-back/src/main/java/com/team/team_collab/TeamCollabApplication.java
@@ -0,0 +1,13 @@
+package com.team.team_collab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCollabApplication.class, args);
+	}
+
+}
---
+++ team-collab-spring-back/src/test/java/com/team/team_collab/TeamCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.team_collab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
